0e004d57d2153b691c20ad86f08e41c5cbca46ee
Kingdon B <kingdon@urmanac.com>
2025-08-17 18:12:53 -0400

    outputs.tf
    Signed-off-by: Kingdon B <kingdon@urmanac.com>

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "admin_username" {
 output "admin_policy_arn" {
   value = module.bootstrap_admin.admin_policy_arn
 }
+
+output "asg_name" { value = module.bastion_ci.asg_name }
+# output "bastion_role_arn" { value = module.bastion_ci.bastion_instance_role_arn }
+# output "terraform_ci_role_arn" { value = module.bastion_ci.terraform_ci_role_arn }
